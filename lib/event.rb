e7e4c48be2a93d32c918821806eb6eef0e55c74e
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-12 14:59:19 -0700

    Good grief

--- lib/event.rb
+++ lib/event.rb
@@ -22,5 +22,47 @@ class Event
       food_truck.inventory.keys.include?(item)
     end
   end
-      
+
+  def available_items
+    @food_trucks.map do |food_truck|
+      food_truck.inventory.keys
+    end.flatten.uniq
+  end
+
+  def total_quantity_of_item(item)
+    total_item = 0
+    @food_trucks.each do |truck|
+      if truck.inventory.keys.include?(item)
+        total_item += truck.inventory[item]
+      end
+    end
+    total_item
+  end
+
+  def item_info(item)
+    info = Hash.new
+    info[:quantity] = total_quantity_of_item(item)
+    info[:food_trucks] = food_trucks_that_sell(item)
+    info
+  end
+
+  def total_inventory
+    total_inventory = Hash.new
+    available_items.each do |item|
+      total_inventory[item] = item_info(item)
+    end
+    total_inventory
+  end
+
+  def overstocked_items
+    overstocked_items = []
+    available_items.each do |item|
+      if (total_inventory[item][:food_trucks].count > 1)
+        overstocked_items << item
+      end
+    end
+  end
+# (total_inventory[item][:food_trucks].count > 1)
+
+
 end
--- test/event_test.rb
+++ test/event_test.rb
@@ -81,4 +81,56 @@ class EventTest < Minitest::Test
     assert_equal expected_item4, event.food_trucks_that_sell(item4)
   end
 
+  def test_sorted_item_list
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected = {item1: {quantity: 100, food_trucks: [food_truck1, food_truck3]},
+                item2: {quantity: 7, food_trucks: [food_truck1]},
+                item4: {quantity: 50, food_trucks: [food_truck2]},
+                item3: {quantity: 35, food_trucks: [food_truck2, food_truck3]}
+                }
+    assert_instance_of Hash, event.total_inventory
+    assert_equal [item1, item2, item4, item3], event.total_inventory.keys
+    assert_equal [item1, item2, item4, item3], event.available_items
+    assert_instance_of Hash, event.item_info(item1)
+    assert_equal 100, event.total_quantity_of_item(item1)
+  end
+
+  def test_overstocked_items
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    assert_equal [item1], event.overstocked_items
+  end 
+
+
 end
